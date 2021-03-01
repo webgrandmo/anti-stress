388adaf50c1fe6adc590e0c6a27525d7f8d25add
Andy Trofimov <webgrandmo@gmail.com>
2021-03-01 22:51:14 +0200

    Dealing with carousel component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,113 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <h1>Dog App</h1>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div id="app">
+		<header-component
+			fullList="Full List"
+			random="Random Doggy"
+			subBreed="Sub Breed"
+			breed="Breed"
+		></header-component>
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<br />
+					<div class="form-group">
+						<button class="btn btn-primary" @click="fetchImg">
+							Get Random Doggy
+						</button>
+					</div>
+
+					<div class="card text-center">
+						<div class="card-body">
+							<div class="doggy-img">
+								<img :src="random_img_url" alt="asdasd" />
+							</div>
+						</div>
+					</div>
+					<br />
+					<div class="card">
+						<div class="card-body">
+							<carousel>
+								<slide v-for="item in images.slice(0, 100)" :key="item.message">
+									<img :src="item" alt="Dog image" />
+								</slide>
+							</carousel>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
+<style lang="css">
+	.doggy-img {
+		width: 300px;
+		height: auto;
+		margin: auto;
+	}
 
-export default {
-  name: 'App',
-  components: {
-    
-  }
-}
-</script>
+	.doggy-img img {
+		max-width: 100%;
+	}
 
-<style src="../node_modules/bootstrap/dist/css/bootstrap-grid.min.css">
+	@media screen and (min-width: 768px) {
+		.doggy-img {
+			width: 600px;
+		}
+	}
 
+	.vs-carousel__arrows {
+		background: rgba(0, 0, 0, 0.3);
+		border: none;
+		border-radius: 50%;
+		color: white;
+		text-indent: -9999px;
+	}
+
+	.vs-carousel__arrows--right {
+		background: url('./assets/next.svg') no-repeat center;
+	}
+	.vs-carousel__arrows--left {
+		background: url('./assets/back.svg') no-repeat center;
+	}
 </style>
+
+<script>
+	import { Carousel, Slide } from 'vue-snap';
+	import 'vue-snap/dist/vue-snap.css';
+	import HeaderComponent from './components/layout/HeaderComponent.vue';
+	export default {
+		name: 'App',
+		components: {
+			HeaderComponent,
+			Carousel,
+			Slide,
+		},
+		data() {
+			return {
+				items: [],
+				images: [],
+				random_img_url: '',
+				breed_img_url: '',
+			};
+		},
+		methods: {
+			async fetchImg() {
+				const result = await fetch(`https://dog.ceo/api/breeds/image/random`);
+				const data = await result.json();
+				this.random_img_url = data.message;
+			},
+		},
+
+		async beforeMount() {
+			const f = await fetch(`https://dog.ceo/api/breeds/image/random`);
+			const data = await f.json();
+			const result = await fetch(`https://dog.ceo/api/breed/hound/images`);
+			const dogs = await result.json();
+			this.random_img_url = data.message;
+			this.images = dogs.message;
+			console.log(dogs);
+			console.log(this.items);
+		},
+	};
+</script>
---
+++ src/components/layout/HeaderComponent.vue
@@ -0,0 +1,62 @@
+<template>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">
+				<img
+					src="../../assets/logo.svg"
+					width="60"
+					height="60"
+					class="d-inline-block align-center"
+					alt=""
+				/>
+				Doggy Anti-stress
+			</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div
+				class="collapse navbar-collapse d-flex justify-content-end"
+				id="navbarNav"
+			>
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#"
+							>{{ random }} <span class="sr-only">(current)</span></a
+						>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">{{ breed }}</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">{{ subBreed }}</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">{{ fullList }}</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<style></style>
+
+<script>
+	export default {
+		name: 'HeaderComponent',
+		props: {
+			random: String,
+			breed: String,
+			subBreed: String,
+			fullList: String,
+		},
+	};
+</script>
